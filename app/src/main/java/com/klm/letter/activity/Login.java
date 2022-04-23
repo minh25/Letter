5ff23301659f888efd33edcfee43784ea452fcaf
minh25 <minh00012258@gmail.com>
2022-04-23 12:16:52 +0700

    add Login, Signup feature

--- app/src/main/java/com/klm/letter/activity/Login.java
+++ app/src/main/java/com/klm/letter/activity/Login.java
@@ -1,5 +1,6 @@
 package com.klm.letter.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,7 +8,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.klm.letter.R;
 
 public class Login extends AppCompatActivity {
@@ -17,16 +23,41 @@ public class Login extends AppCompatActivity {
     private Button button_login;
     private Button button_signup;
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // Init instances
         edit_text_email = findViewById(R.id.edit_text_email);
         edit_text_password = findViewById(R.id.edit_text_password);
         button_login = findViewById(R.id.button_login);
         button_signup = findViewById(R.id.button_signup);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        // Specify instances
+        button_login.setOnClickListener(view -> {
+            String email = edit_text_email.getText().toString();
+            String password = edit_text_password.getText().toString();
+
+            login(email, password);
+        });
         button_signup.setOnClickListener(v -> startActivity(new Intent(v.getContext(), SignUp.class)));
+    }
+
+    private void login(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        Intent intent = new Intent(Login.this, Home.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(Login.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
--- app/src/main/java/com/klm/letter/activity/SignUp.java
+++ app/src/main/java/com/klm/letter/activity/SignUp.java
@@ -1,16 +1,64 @@
 package com.klm.letter.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.klm.letter.R;
 
 public class SignUp extends AppCompatActivity {
+
+    private EditText edit_text_name;
+    private EditText edit_text_email;
+    private EditText edit_text_password;
+    private Button button_signup;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //
+        edit_text_name = findViewById(R.id.edit_text_name);
+        edit_text_email = findViewById(R.id.edit_text_email);
+        edit_text_password = findViewById(R.id.edit_text_password);
+        button_signup = findViewById(R.id.button_signup);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        //
+        button_signup.setOnClickListener(view -> {
+            String name = edit_text_name.getText().toString();
+            String email = edit_text_email.getText().toString();
+            String password = edit_text_password.getText().toString();
+
+            signup(name, email, password);
+        });
+    }
+
+    private void signup(String name, String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        Intent intent = new Intent(SignUp.this, Home.class);
+                        startActivity(intent);
+                    } else {
+                        Log.w("SIGNUP_FAIL", "signInWithEmail:failure", task.getException());
+                        Toast.makeText(SignUp.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
